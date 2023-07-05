2b7f3360f6e037b0f4c6ec8eeb9281bd075ad9dc
Dian Radoslavov <106612784+mokgul@users.noreply.github.com>
2023-07-05 20:43:01 +0300

    succesfully implemented ftp server file upload and a simple design for the upload view

--- Artful-Adventures/ArtfulAdventures.Web/Controllers/PictureController.cs
+++ Artful-Adventures/ArtfulAdventures.Web/Controllers/PictureController.cs
@@ -1,6 +1,11 @@
 ﻿namespace ArtfulAdventures.Web.Controllers
 {
+    using System.Drawing;
+    using System.IO;
+    using System.IO.Pipes;
+    using System.Net;
     using System.Security.Claims;
+    using System.Text;
 
     using ArtfulAdventures.Data;
     using ArtfulAdventures.Data.Models;
@@ -8,8 +13,13 @@
     using ArtfulAdventures.Web.ViewModels.HashTag;
     using ArtfulAdventures.Web.ViewModels.Picture;
 
+    using FluentFTP;
+    using FluentFTP.Client.BaseClient;
+
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
+
+    using static ArtfulAdventures.Common.GeneralApplicationConstants;
 
     public class PictureController : Controller
     {
@@ -30,7 +40,7 @@
         public async Task<IActionResult> Upload()
         {
             var model = await _pictureService.GetPictureAddFormModelAsync();
-            
+
             return View(model);
         }
 
@@ -51,7 +61,7 @@
             {
                 return RedirectToAction("Error", "Home");
             }
-            
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -62,17 +72,58 @@
 
         private async Task<string> UploadFile()
         {
+            //Reads the form data from the request body.
             var form = await Request.ReadFormAsync();
-            var file = form.Files.First();
 
-            var id = file.FileName;
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", id + ".png");
-            using (var stream = new FileStream(path, FileMode.Create))
+            //Gets the first file and saves it to the specified path.
+            var file = form.Files.First();
+            var fileName = file.FileName;
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", fileName);
+
+            //Copy the file to the wwwroot/images folder
+            using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
-            return path;
+
+            //Get the URL of the file to be uploaded
+            var url = Path.Combine(ftpServerUrl, fileName);
+
+            //Upload the file to the FTP server
+            UploadToFtpServer(fileName, filePath);
+
+            //Delete the file from the local folder
+            if (System.IO.File.Exists(filePath))
+                System.IO.File.Delete(filePath);
+
+            return url;
         }
 
+        private static void UploadToFtpServer(string fileName, string filePath)
+        {
+            FtpClient client = new FtpClient();
+            client.Credentials = new NetworkCredential(ftpUserName, ftpPassword);
+            client.Host = ftpServerUrl;
+            client.Port = ftpPort;
+            client.Config.EncryptionMode = FtpEncryptionMode.Explicit;
+            client.ValidateCertificate += new FtpSslValidation(OnValidateCertificate);
+
+            client.Connect();
+            client.UploadFile(filePath, fileName);
+            client.Disconnect();
+        }
+
+        private static void OnValidateCertificate(BaseFtpClient control, FtpSslValidationEventArgs e)
+        {
+            if (e.PolicyErrors != System.Net.Security.SslPolicyErrors.None)
+            {
+                // invalid cert, do you want to accept it?
+                e.Accept = true;
+            }
+            else
+            {
+                e.Accept = true;
+            }
+        }
     }
 }
